b15b7e153e252bf845af2cc1b5174086fe9ab297
Parshv Limbad <103508696+ParshvLimbad@users.noreply.github.com>
2024-07-01 18:12:42 +0530

    Add settings modal open and close functionality and css fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,13 +5,19 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import SettingsIcon from "@mui/icons-material/Settings";
 import { Button } from "./ui/button";
-import SettingsModal from "./SettingsModal";
+import CloseIcon from "@mui/icons-material/Close";
+import { CardTitle } from "./ui/card";
+import { Card } from "@mui/material";
+import { Input } from "./ui/input";
 
 const Header = () => {
   const [modalState, setModalState] = useState(false);
 
   const modalStateTrue = () => {
     setModalState(true);
+  };
+  const modalStateFalse = () => {
+    setModalState(false);
   };
 
   gsap.registerPlugin(useGSAP);
@@ -75,7 +81,22 @@ const Header = () => {
           <SettingsIcon />
         </Button>
       </div>
-      {modalState && <SettingsModal />}
+      {modalState && (
+        <Card className="absolute p-7 ml-auto mr-auto left-0 right-0 top-[25%] w-[30rem] h-[20rem] bg-[#09090B] border-[#27272A]">
+          <div className="flex flex-row justify-between">
+            <CardTitle className="text-[#FAFAFA]">Settings</CardTitle>
+            <button onClick={modalStateFalse}>
+              <CloseIcon className="text-white" />
+            </button>
+          </div>
+          <div className="flex flex-col mt-6 w-full items-center">
+            <div className="w-[20rem] mb-2">
+              <label className="text-[#A1A1AA] self-start">Model</label>
+            </div>
+            <Input className="text-[#DFEEF1] rounded-full px-4 py-1 w-[20rem] outline-none border border-[#27272A] border-2 focus:border-[#17ccf0] ease-in-out duration-300 bg-[#09090B] focus-visible:ring-[none]"></Input>
+          </div>
+        </Card>
+      )}
       <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.0/CustomEase.min.js"></script>
     </nav>
   );
